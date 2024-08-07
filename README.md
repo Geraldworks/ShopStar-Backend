5e9b9cf1c5789096f164fdfa7ba63a46c3577c67
Gerald Ho <97504394+Geraldworks@users.noreply.github.com>
2024-08-07 10:31:17 +0800

    updated readme and other small details

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # ShopStar-Backend
+
+Welcome to **ShopStar-Backend**, the server-side component of the ShopStar application. This backend service handles all CRUD operations and data management for the ShopStar application, providing a robust API to interact with product listings.
+
+## Features
+
+- **RESTful API**: Provides endpoints for creating, reading, updating, and deleting product listings.
+- **SQLite Database**: Utilizes SQLite for lightweight and efficient data storage.
+- **Authentication and Authorization**: Securely manage user access.
+- **Static File Serving**: Deploys static files from the frontend application to deliver a seamless user experience
+
+## Getting Started
+
+To get started with ShopStar-Backend, clone the repository and follow the installation instructions. Ensure you have SQLite installed and configured for database management. The backend also serves static files from the frontend, so make sure to build and include the frontend assets in the project as `dist`.
+
+## Installation Instructions
+
+Run this chunk to install the required libraries
+
+```bash
+npm install
+```
+
+The repository comes with a prepopulated SQLite Database. However if you do need to re-initialise the database, run this command
+
+```bash
+npm run init-db:dev
+```
+
+Finally, run this command to get yourself started in the development environment
+
+```bash
+npm run dev
+```
+
+## Environment File
+
+A `sample.env` file has been added which lists all required environment variables in this project
--- src/utils/initialiseDb.ts
+++ src/utils/initialiseDb.ts
@@ -3,114 +3,194 @@ import db from "./db";
 
 const listings = [
   {
-    createdAt: "2024-07-31T15:58:07.864Z",
-    title: "Investor Operations Executive",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "Ab aliquam blanditiis accusantium praesentium deleniti.",
-    price: 43606
-  },
-  {
-    createdAt: "2024-07-31T13:52:41.360Z",
-    title: "Corporate Assurance Coordinator",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "totam quos repellat",
-    price: 10310
-  },
-  {
-    createdAt: "2024-07-31T16:00:40.983Z",
-    title: "Product Interactions Strategist",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "Ratione ea dolorum vitae tempora ullam quo.",
-    price: 93759
-  },
-  {
-    createdAt: "2024-07-31T08:50:12.187Z",
-    title: "Legacy Creative Administrator",
-    listingImage: "https://loremflickr.com/640/480/cats",
+    createdAt: "2002-06-26T00:28:57.438Z",
+    title: "Chair",
+    listingImage: "https://loremflickr.com/640/480/food",
     description:
-      "Sit est iste omnis ad. Quaerat suscipit porro molestiae eligendi nostrum optio est fugiat animi. Dolor beatae voluptate minus optio.",
-    price: 13515
+      "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+    price: 464.0
   },
   {
-    createdAt: "2024-08-01T00:26:25.174Z",
-    title: "International Usability Liaison",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "veritatis suscipit ullam",
-    price: 94446
-  },
-  {
-    createdAt: "2024-07-31T06:50:30.908Z",
-    title: "International Optimization Assistant",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "tempore nisi provident",
-    price: 13264
-  },
-  {
-    createdAt: "2024-08-01T06:05:08.192Z",
-    title: "International Applications Producer",
-    listingImage: "https://loremflickr.com/640/480/cats",
+    createdAt: "2060-01-01T06:49:09.805Z",
+    title: "Table",
+    listingImage: "https://loremflickr.com/640/480/food",
     description:
-      "Id impedit officia cupiditate vero. Necessitatibus harum ea eius officia praesentium magnam. Esse aperiam velit eum a id. Illo nemo tenetur. Necessitatibus rerum quas.",
-    price: 98617
+      "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    price: 370.0
   },
   {
-    createdAt: "2024-08-01T01:35:55.198Z",
-    title: "Direct Tactics Orchestrator",
-    listingImage: "https://loremflickr.com/640/480/cats",
+    createdAt: "2047-07-21T06:24:46.566Z",
+    title: "Chicken",
+    listingImage: "https://loremflickr.com/640/480/food",
     description:
-      "Sit nostrum enim repellendus earum. Occaecati perspiciatis praesentium consequatur nemo exercitationem cumque animi odit ratione. Adipisci distinctio omnis vitae cupiditate. Magnam culpa aut doloribus ipsa sint fugiat. Repudiandae hic facere quia pariatur reprehenderit corrupti laboriosam nihil debitis. Eveniet enim expedita et voluptatum magni ex cumque architecto et.",
-    price: 67525
+      "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+    price: 227.0
   },
   {
-    createdAt: "2024-08-01T03:42:27.655Z",
-    title: "Corporate Data Technician",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "consectetur sunt minus",
-    price: 19178
-  },
-  {
-    createdAt: "2024-07-31T08:35:48.227Z",
-    title: "Customer Data Executive",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "In maiores atque qui perspiciatis sapiente dolor.",
-    price: 30283
-  },
-  {
-    createdAt: "2024-08-01T00:04:20.123Z",
-    title: "Dynamic Paradigm Producer",
-    listingImage: "https://loremflickr.com/640/480/cats",
+    createdAt: "2091-02-25T11:58:21.827Z",
+    title: "Chips",
+    listingImage: "https://loremflickr.com/640/480/food",
     description:
-      "Reiciendis assumenda dicta ab reprehenderit nisi. Architecto adipisci fugiat soluta pariatur similique porro sint tempora. Aliquid minus sunt maxime quisquam delectus. Expedita esse ducimus minus molestiae eius reiciendis alias.",
-    price: 37981
+      "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+    price: 953.0
   },
   {
-    createdAt: "2024-07-31T08:35:07.938Z",
-    title: "District Interactions Engineer",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "Laborum quasi veniam occaecati iure mollitia alias qui.",
-    price: 5185
-  },
-  {
-    createdAt: "2024-08-01T05:52:42.955Z",
-    title: "District Intranet Designer",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "quasi",
-    price: 41397
-  },
-  {
-    createdAt: "2024-07-31T22:36:53.152Z",
-    title: "Customer Implementation Representative",
-    listingImage: "https://loremflickr.com/640/480/cats",
-    description: "Molestiae tempore amet sequi qui.",
-    price: 21541
-  },
-  {
-    createdAt: "2024-07-31T17:09:46.647Z",
-    title: "Senior Operations Consultant",
-    listingImage: "https://loremflickr.com/640/480/cats",
+    createdAt: "2054-10-01T07:04:52.834Z",
+    title: "Fish",
+    listingImage: "https://loremflickr.com/640/480/food",
     description:
-      "Quasi similique error eaque iure quas sapiente non dolore modi. Maxime nam repudiandae explicabo veritatis. Eligendi eum vel ea ipsa hic temporibus nam earum vel. Maxime non ex.\nQuasi ipsa facere voluptatibus animi repellat quaerat molestias ea ducimus. Impedit adipisci quis iure doloremque. Deleniti fugiat illo quasi quos nihil neque nam nulla.\nSuscipit similique debitis quidem vitae similique eaque ullam qui. Dolorum ad aliquam a quas magnam. Nulla consequatur quisquam alias beatae incidunt sint perferendis dolores sit. Molestias nisi possimus dolores. Suscipit nisi voluptatem quae reprehenderit reprehenderit.",
-    price: 66820
+      "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
+    price: 405.0
+  },
+  {
+    createdAt: "2010-11-19T11:40:23.047Z",
+    title: "Keyboard",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    price: 437.0
+  },
+  {
+    createdAt: "2058-12-05T22:17:46.116Z",
+    title: "Bike",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+    price: 576.0
+  },
+  {
+    createdAt: "2070-04-14T12:19:05.622Z",
+    title: "Cheese",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+    price: 121.0
+  },
+  {
+    createdAt: "1996-01-31T23:34:07.547Z",
+    title: "Pizza",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    price: 595.0
+  },
+  {
+    createdAt: "2037-08-01T18:49:46.441Z",
+    title: "Hat",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
+    price: 263.0
+  },
+  {
+    createdAt: "2067-12-17T19:25:46.875Z",
+    title: "Shirt",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
+    price: 202.0
+  },
+  {
+    createdAt: "2095-04-27T15:19:07.234Z",
+    title: "Gloves",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description: "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit",
+    price: 488.0
+  },
+  {
+    createdAt: "2004-12-24T16:13:39.686Z",
+    title: "Bike",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    price: 567.0
+  },
+  {
+    createdAt: "2005-08-05T23:10:58.087Z",
+    title: "Chips",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    price: 883.0
+  },
+  {
+    createdAt: "2069-10-13T22:46:50.854Z",
+    title: "Bacon",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description: "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit",
+    price: 675.0
+  },
+  {
+    createdAt: "2034-07-19T22:10:33.512Z",
+    title: "Shoes",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+    price: 821.0
+  },
+  {
+    createdAt: "2068-02-28T22:29:09.565Z",
+    title: "Computer",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+    price: 387.0
+  },
+  {
+    createdAt: "1998-03-31T22:44:47.494Z",
+    title: "Pants",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+    price: 190.0
+  },
+  {
+    createdAt: "2006-10-06T07:15:49.521Z",
+    title: "Chips",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+    price: 834.0
+  },
+  {
+    createdAt: "2003-02-15T13:22:40.258Z",
+    title: "Bike",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    price: 680.0
+  },
+  {
+    createdAt: "2037-12-10T03:28:17.521Z",
+    title: "Chair",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+    price: 127.0
+  },
+  {
+    createdAt: "1994-10-01T15:39:55.284Z",
+    title: "Tuna",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    price: 955.0
+  },
+  {
+    createdAt: "2047-08-30T06:57:32.332Z",
+    title: "Bacon",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+    price: 218.0
+  },
+  {
+    createdAt: "1998-02-23T11:28:59.911Z",
+    title: "Hat",
+    listingImage: "https://loremflickr.com/640/480/food",
+    description:
+      "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+    price: 590.0
   }
 ];
 
